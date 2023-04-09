c06a900b643071aa06cc31205c2788bf136bbd1e
Rizki Perdana <perdana.rizki16@gmail.com>
2023-04-09 19:46:46 +0700

    api prefix and config mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import UserModule from './modules/user/user.module';
 
-const defaultDb = 'mongodb://mongodb:27017/nest-profile';
+const defaultDb = 'mongodb://admin:admin@localhost:27017';
 const MONGO_URL = process.env.MONGODB_URL || defaultDb;
 
 @Module({
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const APP_PORT = process.env.APP_PORT || 3000;
+  app.setGlobalPrefix('api');
   await app.listen(APP_PORT);
 }
 bootstrap();
